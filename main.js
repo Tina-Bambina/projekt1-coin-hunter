fcce54f47bc296ae5c615712ce13e40d9fa8b54d
Tina-Bambina <speleerska@gmail.com>
2022-03-03 17:17:08 +0400

    prvni pokus o pohyb

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+let vzdalenost = 50;
+let pan = document.querySelector('#panacek');
+
+window.addEventListener('nacteni', () => {
+	pan.style.position = 'absolute';
+	pan.style.left = 0;
+	pan.style.top = 0;
+})
+
+window.addEventListener('key', (e) => {
+	switch(e.key){
+		case 'ArrowLeft' : pan.style.left = parseInt(pan.style.left) - vzdalenost + 'px' ; break;
+		case 'ArrowRight' : pan.style.left = parseInt(pan.style.left) + vzdalenost + 'px' ; break;
+		case 'ArrowUp' : pan.style.top = parseInt(pan.style.top) - vzdalenost + 'px' ; break;
+		case 'ArrowDown' : pan.style.top = parseInt(pan.style.top) + vzdalenost + 'px'; break;
+	}
+});
+
+
+function PohybJedna() {
+    let pohni = document.getElementById('#panacek');
+
+    pohni.style.left = '420px';
+    pohni.style.top = '260px';
+}
